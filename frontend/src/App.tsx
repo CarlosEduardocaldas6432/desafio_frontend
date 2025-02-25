c91184aa4930ba5e46c0eb5fc365c5de1669caa3
mataporco <matapig6@gmail.com>
2025-02-24 21:44:57 -0300

    o template do cabeçalho e da barra de pesquisa foram feitos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,22 @@ function App() {
 
   return (
     <>
-      
+      <header>
+        <img className='logo' src="/logo.svg" alt="logo" />
+      </header>
+
+      <div className="div_pesquisa">
+        <h1>Fucionários</h1>
+        <form action="/pesquisar" method="GET" className="barra-pesquisa">
+    <div className="input-container">
+        <input type="text" name="q" placeholder="Pesquisar" required></input>
+         
+    </div>
+    <button type="submit"> <img src="/icons/lupa.svg" alt="botão de pesquisa" /></button>
+</form>
+        
+
+      </div>
     </>
   )
 }
